923e7bbcc54d44c66b655c55cc859dd6c87ee0b9
Tasuku Okuno <49193846+Tasuku-Okuno@users.noreply.github.com>
2025-12-29 17:56:09 +0900

    Add tab resize settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 vim.opt.termguicolors = false
 vim.opt.syntax = "on"
 vim.opt.expandtab = true      -- タブをスペースに変換
@@ -13,5 +16,11 @@ vim.cmd([[colorscheme darkblue]])
 vim.opt.clipboard = "unnamedplus"
 vim.keymap.set({ "n", "x" }, "c", '"_c', { noremap = true, silent = true })
 vim.keymap.set("n", "C", '"_C', { noremap = true, silent = true })
+
+-- ウィンドウサイズの変更
+vim.keymap.set("n", "<leader>wh", ":vertical resize -5<CR>", { silent = true })
+vim.keymap.set("n", "<leader>wl", ":vertical resize +5<CR>", { silent = true })
+vim.keymap.set("n", "<leader>wj", ":resize -5<CR>", { silent = true })
+vim.keymap.set("n", "<leader>wk", ":resize +5<CR>", { silent = true })
 
 require("config.lazy")
